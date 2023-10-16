fd19777792080439982e3424091da28e68a3c6e4
yann.champeau <yann.champeau@codefirst.iut.uca.fr>
2023-10-16 09:57:26 +0200

    :alembic: Add Graph Functions

---
+++ Front-end/NodeMunch/src/functions/Graphs.ts
@@ -0,0 +1,34 @@
+import '../../node_modules/typescript'
+
+class NodeP{
+    posX : number;
+    posY : number;
+    constructor(posX : number, posY : number){
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    afficher(): void{
+        console.log(this.posX, " test ", this.posY)
+    }
+}
+
+class Graph{
+    nodes : Array<NodeP>;
+
+    constructor(type="auto"){
+        if(type=="auto") {
+            this.nodes = [new NodeP(2, 1),new NodeP(2, 1), new NodeP(3, 1)];
+        } else {
+            this.nodes = [];
+        }
+    }
+
+    afficherG(): void {
+        this.nodes.forEach((node) => console.log(node.afficher));
+    }
+}
+
+let grap = new Graph("auto");
+
+grap.afficherG();
