3427d2924fb2627156e548ce6624694490d8ecac
Aldeberian <105972830+Aldeberian@users.noreply.github.com>
2023-10-17 11:51:20 +0200

    :construction: Add links and nodes

--- Front-end/NodeMunch/src/functions/Graphs.ts
+++ Front-end/NodeMunch/src/functions/Graphs.ts
@@ -1,7 +1,7 @@
 import '../../node_modules/typescript'
 
 //Named NodeP because you can't name something Node in typescript, it's taken already
-class NodeP{
+export class NodeP{
     id : number;
     posX : number;
     posY : number;
@@ -13,7 +13,7 @@ class NodeP{
     }
 
     printCoordinates(): string{
-        return 'Node : ' + this.id + 'Position X : ' + this.posX + ', Position Y : ' + this.posY;
+        return 'Node : ' + this.id + ' Position X : ' + this.posX + ', Position Y : ' + this.posY;
     }
 }
 
@@ -25,7 +25,7 @@ let autoTabNode : Array<NodeP> = [
 ];
 
 //Takes two Ids of node
-class Link {
+export class Link {
     id1 : number;
     id2 : number;
     
@@ -46,7 +46,7 @@ let autoTabLink : Array<Link> = [
     new Link(1, 3)
 ];
 
-export default class Graph{
+export class Graph{
     nodes : Array<NodeP>;
     links : Array<Link>;
 
@@ -74,5 +74,47 @@ export default class Graph{
         this.links.forEach((link) => {
             console.log(link.printLink());
         });
+    }
+
+    //Todo Handle error messages in error cases
+    addNode(node : NodeP) {
+
+        if(this.nodes.find(item => item.id == node.id)) {
+            console.log("ERROR : This id already exists in the graph. The node : " + node.printCoordinates() + " was not added");
+        }
+        else if( (this.nodes.find(item => item.posX == node.posX) ) && (this.nodes.find(item => item.posY == node.posY ))) {
+            console.log("ERROR : A node is the graph already got those coordinates. The node : " + node.printCoordinates() + " was not added");
+        }
+        else {
+            this.nodes.push(node);
+        }        
+    }
+
+    addLink(link : Link) {
+        
+        if( !this.nodes.find(item => item.id == link.id1)) {
+            console.log("ERROR : The node " + link.id1 + " does not exist in the graph");
+        }
+        else if( !this.nodes.find(item => item.id == link.id1)) {
+            console.log("ERROR : The node " + link.id2 + " does not exist in the graph");
+        } 
+        else if( ( 
+                (this.links.find(item => item.id1 == link.id1) ) 
+                && 
+                (this.links.find(item => item.id2 == link.id2 )) 
+            )
+            || 
+            (
+                (this.links.find(item => item.id2 == link.id1) ) 
+                && 
+                (this.links.find(item => item.id1 == link.id2 )) 
+            )
+        ) 
+        {
+            console.log("ERROR : A link from " + link.id1 + " to " + link.id2 + " already exists. The link was not added");
+        }
+        else {
+            this.links.push(link);
+        }
     }
 }
--- Front-end/NodeMunch/src/pages/PlayPage.tsx
+++ Front-end/NodeMunch/src/pages/PlayPage.tsx
@@ -1,10 +1,16 @@
-import Graph from '../functions/Graphs.ts';
+import * as G from '../functions/Graphs.ts';
 
-const grap = new Graph();
+const grap = new G.Graph();
+const node = new G.NodeP(4, 1, 3);
+const link = new G.Link(1, 4)
 
 function Play() {
     const handleButtonClick = () => {
         grap.printPosition();
+        grap.addNode(node);
+        grap.printPosition();
+        grap.printLinks();
+        grap.addLink(link);
         grap.printLinks();
     }
 
