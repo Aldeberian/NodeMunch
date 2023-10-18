1d6efd311be6d34ffcb5ffb0de111def8e59bcc2
baptiste.brunet <babrunet@IUTCLINFB2012L.etu.uca.local>
2023-10-18 15:19:33 +0200

    try pushing from a pratical class

---
+++ Web_Client/controller/AdminController.php
@@ -0,0 +1,28 @@
+<?php
+
+class AdminController
+{
+
+    public function __construct() {
+
+
+    }
+
+    public function deleteAGraph(int $idGrah) {
+
+
+    }
+
+    public function banUser(int $idUser) {
+
+
+    }
+
+    public function unBanUser(int $idUser) {
+
+
+
+    }
+
+
+}
