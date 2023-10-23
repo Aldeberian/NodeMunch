552e65368e33d4439525443c66f1659a586d4242
lola.chalmin <lochalmin@IUTCLINFC2108L.uca.local>
2023-10-23 13:16:30 +0200

    :hammer: added error.php and some other validation function

--- Web_Client/config/Validation.php
+++ Web_Client/config/Validation.php
@@ -1,5 +1,7 @@
+<?php
 class Validation{
-    public static function validAction(){
+    public static function validAction()
+    {
         $action = (isset($_REQUEST['action'])) ? 'none' : $_REQUEST['action'];
         if ($action == 'none')
         {
@@ -7,12 +9,28 @@ class Validation{
         }        
     }
 
-    public static function validName(string $pseudo){
+    public static function validPseudo(string $pseudo)
+    {
         if (!isset($pseudo) or $pseudo == '')
         {
             $tabError[] = 'pseudoError';
             $pseudo = '';
-        }
-        
+        }        
     }
+
+    public static function validMail(string $mail)
+    {
+        if (!isset($mail) or $mail == '')
+        {
+            $tabError[] = 'emptyMailError';
+            $mail = '';
+        }
+        if (!filter_var($mail, FILTER_VALIDATE_MAIL))
+        {
+            $tabError[] = 'badMailError';
+            $mail = '';
+        }
+    }
+
+    //require_once('errors.php'); don't know where to put this 
 }
