3ba793b472ecc5161e6cc53ba96fef0547efc3b7
Nicolas BLONDEAU <nicolas.blondeau@etu.uca.fr>
2023-10-25 10:26:22 +0200

    Mise à jour de 'Front-end/NodeMunch/src/pages/ParametersPage.tsx'

--- Front-end/NodeMunch/src/pages/ParametersPage.tsx
+++ Front-end/NodeMunch/src/pages/ParametersPage.tsx
@@ -11,9 +11,9 @@ function ParametersPage(){
       <WavyBackground/>
       <div className="container">
         <div className="elements">
-          <Button url="" color="orange">On</Button>
+          <Button url="" color="orange">Test button</Button>
           <Button url="" color="orange">This is another button</Button>
-          <Button url="" color="#3498db">Maybe another button</Button>
+          <Button url="" color="#3498db">Or is it?</Button>
           <table>
             <td><Button url="/" color="green">Save</Button></td>
             <td>  </td>
