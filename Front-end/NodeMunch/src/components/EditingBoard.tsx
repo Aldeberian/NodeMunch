987a32e91dd93e6af827b38deb95c08fda5decb5
cleo.eiras <cleo.eiras@etu.uca.fr>
2023-10-25 09:58:15 +0200

    :construction: Almost editing board

--- Front-end/NodeMunch/src/components/EditingBoard.tsx
+++ Front-end/NodeMunch/src/components/EditingBoard.tsx
@@ -1,20 +1,57 @@
 import Graph from '../functions/Graph';
 import Link from '../functions/Link';
 import NodeP from '../functions/Node';
+import drawGraph from '../functions/DrawingGraph';
+import React, { useState } from 'react';
 
 import './EditingBoard.css'
 
-//Component that displays the graph during the game
-let grap = new Graph("auto");
+let graph = new Graph("custom");
 
-interface boardStatus {
-    status : boolean;
+const nodeClicked = () => {
+    console.log("test")
 }
 
 export default function EditBoard() {
+    const [position, setPosition] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+
+    const [idCustom, setId] = useState<string>("0");
+    
+    const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+        const editBoard = document.getElementById("editingBoard");
+
+        if(!editBoard) return;
+        const x = e.clientX - editBoard.getBoundingClientRect().left;
+        const y = e.clientY - editBoard.getBoundingClientRect().top;
+        setPosition({ x, y });
+    };
+
+    const boardClicked : React.FC = () =>  {
+    
+        let newNode = new NodeP( idCustom, position.x, position.y);
+
+        setId((Number(idCustom)+1).toString());
+    
+        graph.addNode(newNode);
+
+        return (
+            <svg height="100%" width="100%">        
+            <circle
+            cx={position.x}
+            cy={position.y}
+            r={2}
+            fill="red"
+            /></svg>
+        )
+
+        // console.log(graph);
+    
+        // return ( drawGraph(graph, { eventOnClick: nodeClicked }) )
+    };
+
     return (
-        <div id='editingBoard'>
-            <h1>Edit Board</h1>
-        </div>
+      <div id="editingBoard" onMouseMove={handleMouseMove} onClick={boardClicked}>
+
+      </div>
     )
 }
